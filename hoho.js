821dd3ce281c51c96b3ce75b062de4cd84607403
nguyenquochuy <s3818764@rmit.edu.vn>
2022-05-27 21:03:13 +0700

    2

---
+++ hoho.js
@@ -0,0 +1,17 @@
+console.log("0");
+console.log("1");
+
+console.log("haha");
+
+console.log("1");
+
+console.log("1");
+
+console.log("1");
+
+console.log("1");
+
+console.log("3112312");
+console.log("3112312");
+
+console.log("3112312");
